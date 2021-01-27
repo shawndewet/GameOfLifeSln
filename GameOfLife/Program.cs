a7be1a9dd8aec2ec18cb4b97c68d6ec6f032fe5d
Shawn de Wet <shawndewet@codewell.co.za>
2021-01-27 07:17:57 +0200

    Console UI working

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -6,7 +6,42 @@ namespace GameOfLife
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var width = 0;
+            var height = 0;
+            var generations = 0;
+            Console.WriteLine("Welcome to Shawn's Game of Life.");
+            Console.WriteLine("Enter your game parameters:");
+            Console.WriteLine("Width       : ");
+            Console.WriteLine("Height      : ");
+            Console.WriteLine("Generations : ");
+            Console.CursorTop = 0;
+            Console.SetCursorPosition(14, 2);
+            width = Int32.Parse(Console.ReadLine());
+            Console.SetCursorPosition(14, 3);
+            height = Int32.Parse(Console.ReadLine());
+            Console.SetCursorPosition(14, 4);
+            generations = Int32.Parse(Console.ReadLine());
+
+            var game = new Library.Game(width, height, generations);
+
+            Console.CursorVisible = false;
+            Console.SetWindowSize(width, height);
+            Console.SetBufferSize(width, height);
+
+            game.Start((grid)=>
+            {
+                for (int c = 1; c <= width; c++)
+                    for (int r = 1; r <= height; r++)
+                    {
+                        var x = c - 1;
+                        var y = r - 1;
+                        Console.SetCursorPosition(x, y);
+                        var cell = grid[x, y];
+                        Console.Write(cell.Alive ? "X" : ".");
+                    }
+            });
+
+            Console.ReadLine();
         }
     }
 }
